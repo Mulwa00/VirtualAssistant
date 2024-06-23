940f3791fe3d261b1cb9bf9ef63abf430a2c9519
Patrick Mulwa <patrickmulwa012@gmail.com>
2024-06-23 15:22:28 +0300

    added code

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pyttsx3 as p 
 import speech_recognition as sr
 
+from selenium_web import Infow, infow
+
 engine = p.init()
 rate=engine.getProperty('rate')
 engine.setProperty('rate',165)
@@ -28,3 +30,25 @@ if "what" and "about" and "you" in text:
     speak("I am having a good day sir")
 speak("What can i do for you?")
 
+with sr.Microphone() as source:
+    r.energy_threshold=12000
+    r.adjust_for_ambient_noise(source,1.2)
+    print("listening...")
+    audio = r.listen(source)
+    text2 = r.recognize_google(audio)
+
+if "information" in text2:
+    speak("you need information related to which topic?")
+
+    with sr.Microphone() as source:
+        r.energy_threshold=12000
+        r.adjust_for_ambient_noise(source,1.2)
+        print("listening...")
+        audio = r.listen(source)
+        infor = r.recognize_google(audio)
+    speak("searching{}inwikipidea".format())
+
+# Example usage
+if __name__ == "__main__":
+    info = Infow()
+    info.get_info("Python programming")
--- selenium_web.py
+++ selenium_web.py
@@ -1,12 +1,23 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager # type: ignore
 
-class infow():
+class Infow():
     def __init__(self):
-        self.driver = webdriver.Chrome(executable_path="C:\Users\abdul\Desktop\chromedriver-win64\chromedriver.exe")
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-    def get_info(self,query):
-        self.query=query
+    def get_info(self, query):
+        self.query = query
         self.driver.get(url='https://www.wikipedia.org/')
+        search = self.driver.find_element(By.XPATH, '//*[@id="searchInput"]')
+        search.click()
+        search.send_keys(query)
+        enter = self.driver.find_element(By.XPATH, '//*[@id="search-form"]/fieldset/button')
+        enter.click()
 
-assist=infow()
-assist.get_info("hello") 
+# Example usage
+if __name__ == "__main__":
+    info = Infow()
+    info.get_info("Python programming")
+
